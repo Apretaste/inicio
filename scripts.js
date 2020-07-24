19d0df4c81dc6212a176af7add92b0169f6ff473
Salvi Pascual <salvi.pascual@gmail.com>
2020-07-23 23:37:04 -0400

    almost finished

--- scripts.js
+++ scripts.js
@@ -1,36 +1,39 @@
-$(document).ready(function () {
+$(document).ready(function(){
 	$('select').formSelect();
 });
 
-function options() {
-	$('.drop-down .options').slideDown('fast');
-}
+// filter by service category
+function filtrar(e) {
+	// get the category
+	var category = $(e).attr('data');
 
-function select(element) {
-	// get text and value from element
-	var text = $(element).find('.text').text();
-	var category = $(element).attr('value');
+	// highlight the category
+	$('.filter').addClass('hidden');
+	$(e).find('.filter').removeClass('hidden');
 
-	// show all categories
-	if(category == 'all') {
-		$('.service').slideDown();
-	} 
-	// filter by category name
-	else {
-		$('.service').hide();
-		$('.'+category).slideDown();		
+	// show the search bar
+	if(category == 'buscar') {
+		$('.service').slideDown('fast');
+		$('#buscar').focus();
+		return false;
 	}
 
-	// hide the options bar
-	$('.drop-down .options').hide();
+	// show all categories
+	if(category == 'todos') {
+		$('.service').slideDown('fast');
+	}
 
-	// add text to the input
-	$('#filter').val(text);
+	// filter by category
+	else {
+		$('.service').hide();
+		$('.'+category).slideDown('fast');		
+	}
 }
 
-function filter(element) {
-	// get text to filter by
-	var text = cleanUpSpecialChars($('#filter').val().toLowerCase());
+// search for a service on the list
+function buscar() {
+	// get text to search by
+	var text = cleanUpSpecialChars($('#buscar').val().toLowerCase());
 
 	$('.service').show().each(function(i, e) {
 		// get the caption
@@ -43,12 +46,8 @@ function filter(element) {
 	})
 }
 
-function clean() {
-	$('#filter').val('');
-}
-
-function cleanUpSpecialChars(str)
-{
+// clean special chars
+function cleanUpSpecialChars(str) {
 	return str
 		.replace(/Á/g,"A").replace(/a/g,"a")
 		.replace(/É/g,"E").replace(/é/g,"e")
@@ -57,4 +56,38 @@ function cleanUpSpecialChars(str)
 		.replace(/Ú/g,"U").replace(/ú/g,"u")
 		.replace(/Ñ/g,"N").replace(/ñ/g,"n")
 		.replace(/[^a-z0-9]/gi,''); // final clean up
+}
+
+// activate or deactivate widget
+function toogleWidget(widget) {
+	// change the icon
+	if($(widget).hasClass('fa-check-circle')) {
+		$(widget).removeClass('fa-check-circle').addClass('fa-circle');
+	} else {
+		$(widget).removeClass('fa-circle').addClass('fa-check-circle');
+	}
+}
+
+// save widgets
+function widgetSave() {
+	// get widgets to save
+	var widgets = [];
+	$('.widget-action').each(function() {
+		var widget = $(this);
+		if($(widget).hasClass('fa-check-circle')) {
+			widgets.push(widget.attr('data'));
+		}
+	});
+
+	// get favorite services
+	var favorites = $('#favoritos').val().slice(0, 4).join(',');
+
+	// submit widgets to be saved
+	apretaste.send({
+		command: 'INICIO SALVAR',
+		data: {
+			'widgets': widgets,
+			'favorites': favorites
+		}
+	});
 }
--- service.php
+++ service.php
@@ -8,15 +8,107 @@ use Framework\Database;
 class Service
 {
 	/**
-	 * Main function
+	 * Home screen for the app, list of widgets and services
 	 *
 	 * @param Request $request
 	 * @param Response $response
-	 * @throws Alert
 	 * @author salvipascual
 	 */
-	public function _main(Request $request, Response $response)
+	public function _main (Request $request, Response $response)
 	{
+		// get list of widgets
+		$preferences = Database::queryFirst("SELECT widgets, favorite_services FROM person WHERE id = {$request->person->id}");
+
+		// get data for the widgets
+		$widgets = [];
+
+		// social
+		if(strpos($preferences->widgets, 'social') !== false) {
+			$widgets['social'] = (Object) [
+				'amigos' => 112,
+				'ranking' => 412,
+				'mensajes' => 1652,
+				'retos' => 12,
+			];
+		}
+
+		// online
+		if(strpos($preferences->widgets, 'online') !== false) {
+			$widgets['online'] = (Object) [
+				'dias' => 112,
+				'semana' => 3,
+			];
+		}
+
+		// experiencia
+		if(strpos($preferences->widgets, 'experiencia') !== false) {
+			$widgets['experiencia'] = (Object) [
+				'experiencia' => 1142,
+				'nivel' => 'Diamante',
+			];
+		}
+
+		// pizarra
+		if(strpos($preferences->widgets, 'pizarra') !== false) {
+			$widgets['pizarra'] = (Object) [
+				'like' => 912,
+				'comment' => 123,
+			];
+		}
+
+		// amuletos
+		if(strpos($preferences->widgets, 'amuletos') !== false) {
+			$widgets['amuletos'] = (Object) [
+				'one' => 'camera',
+				'two' => 'heart-broken',
+				'three' => 'bomb',
+			];
+		}
+
+		// ayuda
+		if(strpos($preferences->widgets, 'ayuda') !== false) {
+			$widgets['ayuda'] = (Object) [
+				'tickets' => 123,
+			];
+		}
+
+		// rifa
+		if(strpos($preferences->widgets, 'rifa') !== false) {
+			$widgets['rifa'] = (Object) [
+				'fecha' => 'Nov 12',
+				'tickets' => 123,
+			];
+		}
+
+		// bolita
+		if(strpos($preferences->widgets, 'bolita') !== false) {
+			$widgets['bolita'] = (Object) [
+				'day_one' => '12',
+				'day_two' => '100',
+				'day_three' => '1',
+				'night_one' => '23',
+				'night_two' => '65',
+				'night_three' => '98',
+			];
+		}
+
+		// piropazo
+		if(strpos($preferences->widgets, 'piropazo') !== false) {
+			$widgets['piropazo'] = (Object) [
+				'parejas' => 12,
+				'likes' => 123,
+			];
+		}
+
+		// noticia
+		if(strpos($preferences->widgets, 'noticia') !== false) {
+			$widgets['noticia'] = (Object) [
+				'titulo' => trim(substr('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incident', 0, 80)) . '...',
+				'canal' => 'Diario de Cuba',
+				'comment' => 123,
+			];
+		}
+
 		// get list of services
 		$services = Database::query("
 			SELECT A.name, A.caption, A.icon, A.category, IFNULL(B.count, 0) AS count
@@ -26,25 +118,31 @@ class Service
 			WHERE A.listed = 1 AND A.active = 1
 			ORDER BY A.name ASC");
 
-		// create the list of categories
+		// get services categories
+		$cats = Database::queryCache("
+			SELECT COUNT(*) AS cnt, category 
+			FROM service
+			WHERE listed = 1 AND active = 1
+			GROUP BY category");
+
+		// organize the categories
 		$categories = [];
-		foreach ($services as $c) {
-			if (isset($categories[$c->category])) {
-				$categories[$c->category]->count++;
-			} else {
-				$category = new \stdClass();
-				$category->code = $c->category;
-				$category->name = Core::$serviceCategories[$c->category];
-				$category->count = 1;
-				$categories[$c->category] = $category;
+		foreach ($cats as $key => $value) {
+			$categories[$value->category] = $value->cnt;
+		}
+
+		// get the favorite services
+		$favorites = [];
+		foreach ($services as $item) {
+			if(strpos($preferences->favorite_services, $item->name) !== false) {
+				$favorites[] = $item;
 			}
 		}
 
-		// sort the categories alphabetically
-		ksort($categories);
-
 		// create the content array
 		$content = [
+			'widgets' => $widgets,
+			'favorites' => $favorites,
 			'services' => $services,
 			'categories' => $categories
 		];
@@ -52,5 +150,55 @@ class Service
 		// create response
 		$response->setCache('month');
 		$response->setTemplate('home.ejs', $content);
+	}
+
+	/**
+	 * List of widgets to edit
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @author salvipascual
+	 */
+	public function _widgets (Request $request, Response $response)
+	{
+		// get list of widgets
+		$preferences = Database::queryFirst("SELECT widgets, favorite_services FROM person WHERE id = {$request->person->id}");
+
+		// get list of services
+		$services = Database::query("SELECT name, caption FROM service WHERE listed = 1 AND active = 1 ORDER BY name ASC");
+
+		// create the content array
+		$content = [
+			'services' => $services,
+			'widgets' => $preferences->widgets,
+			'favorites' => $preferences->favorite_services
+		];
+
+		// create response
+		$response->setCache();
+		$response->setTemplate('widgets.ejs', $content);
+	}
+
+	/**
+	 * List of widgets to edit
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @author salvipascual
+	 */
+	public function _salvar (Request $request, Response $response)
+	{
+		// save the widgets
+		$widgets = implode(',', $request->input->data->widgets);
+		$favorites = $request->input->data->favorites;
+
+		// save widgets to the user profile
+		Database::queryFirst("
+			UPDATE person 
+			SET widgets = '$widgets', favorite_services = '$favorites'
+			WHERE id = {$request->person->id}");
+
+		// create response
+		return $this->_main($request, $response);
 	}
 }
