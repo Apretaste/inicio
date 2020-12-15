812055ab3736a41c06e2554a4e8b27fddd05f593
Salvi Pascual <salvi.pascual@gmail.com>
2020-12-15 11:39:06 -0500

    new favoritos

--- scripts.js
+++ scripts.js
@@ -1,23 +1,56 @@
+/*!
+ * long-press-event - v2.4.2
+ * Pure JavaScript long-press-event
+ * https://github.com/john-doherty/long-press-event
+ * @author John Doherty <www.johndoherty.info>
+ * @license MIT
+ */
+!function(e,t){"use strict";var n=null,a="ontouchstart"in e||navigator.MaxTouchPoints>0||navigator.msMaxTouchPoints>0,i=a?"touchstart":"mousedown",o=a?"touchend":"mouseup",m=a?"touchmove":"mousemove",r=0,u=0,s=10,c=10;function l(e){var n;d(),n=e,e=a&&n.touches&&n.touches[0]?n.touches[0]:n,this.dispatchEvent(new CustomEvent("long-press",{bubbles:!0,cancelable:!0,detail:{clientX:e.clientX,clientY:e.clientY},clientX:e.clientX,clientY:e.clientY,offsetX:e.offsetX,offsetY:e.offsetY,pageX:e.pageX,pageY:e.pageY,screenX:e.screenX,screenY:e.screenY}))||t.addEventListener("click",function e(n){t.removeEventListener("click",e,!0),function(e){e.stopImmediatePropagation(),e.preventDefault(),e.stopPropagation()}(n)},!0)}function v(a){d(a);var i=a.target,o=parseInt(function(e,n,a){for(;e&&e!==t.documentElement;){var i=e.getAttribute(n);if(i)return i;e=e.parentNode}return a}(i,"data-long-press-delay","1500"),10);n=function(t,n){if(!(e.requestAnimationFrame||e.webkitRequestAnimationFrame||e.mozRequestAnimationFrame&&e.mozCancelRequestAnimationFrame||e.oRequestAnimationFrame||e.msRequestAnimationFrame))return e.setTimeout(t,n);var a=(new Date).getTime(),i={},o=function(){(new Date).getTime()-a>=n?t.call():i.value=requestAnimFrame(o)};return i.value=requestAnimFrame(o),i}(l.bind(i,a),o)}function d(t){var a;(a=n)&&(e.cancelAnimationFrame?e.cancelAnimationFrame(a.value):e.webkitCancelAnimationFrame?e.webkitCancelAnimationFrame(a.value):e.webkitCancelRequestAnimationFrame?e.webkitCancelRequestAnimationFrame(a.value):e.mozCancelRequestAnimationFrame?e.mozCancelRequestAnimationFrame(a.value):e.oCancelRequestAnimationFrame?e.oCancelRequestAnimationFrame(a.value):e.msCancelRequestAnimationFrame?e.msCancelRequestAnimationFrame(a.value):clearTimeout(a)),n=null}"function"!=typeof e.CustomEvent&&(e.CustomEvent=function(e,n){n=n||{bubbles:!1,cancelable:!1,detail:void 0};var a=t.createEvent("CustomEvent");return a.initCustomEvent(e,n.bubbles,n.cancelable,n.detail),a},e.CustomEvent.prototype=e.Event.prototype),e.requestAnimFrame=e.requestAnimationFrame||e.webkitRequestAnimationFrame||e.mozRequestAnimationFrame||e.oRequestAnimationFrame||e.msRequestAnimationFrame||function(t){e.setTimeout(t,1e3/60)},t.addEventListener(o,d,!0),t.addEventListener(m,function(e){var t=Math.abs(r-e.clientX),n=Math.abs(u-e.clientY);(t>=s||n>=c)&&d()},!0),t.addEventListener("wheel",d,!0),t.addEventListener("scroll",d,!0),t.addEventListener(i,function(e){r=e.clientX,u=e.clientY,v(e)},!0)}(window,document);
+
+// when document ready
 $(document).ready(function(){
+	// add select on start
 	$('select').formSelect();
+
+	// listen for the long-press event
+	$('.long-press').each(function() {
+		this.addEventListener('long-press', function(e) {
+			e.preventDefault();
+			toggleFavorite($(this).attr('id'));
+		});
+	});
+
+	// filter services
+	if(typeof favorites != 'undefined') {
+		var filter = favorites ? 'favoritos' : 'todos';
+		filtrar(filter);
+	}
 });
 
 // filter by service category
-function filtrar(e) {
-	// get the category
-	var category = $(e).attr('data');
-
+function filtrar(category) {
 	// highlight the category
 	$('.filter').addClass('hidden');
-	$(e).find('.filter').removeClass('hidden');
+	$('#'+category).find('.filter').removeClass('hidden');
 
 	// scroll to the filters
 	$('html, body').animate({scrollTop: $('#filters').offset().top}, 1000);
 
+	// show msg only on the favorite category
+	$('#empty-note').hide();
+
 	// show the search bar
 	if(category == 'buscar') {
 		$('.service').slideDown('fast');
-		$('#buscar').focus();
+		$('#buscar input').focus();
+		return false;
+	}
+
+	// show favorites
+	if(category == 'favoritos') {
+		$('.service').hide();
+		$('.favorite').slideDown('fast');
+		updateFavoriteCount();
 		return false;
 	}
 
@@ -36,7 +69,7 @@ function filtrar(e) {
 // search for a service on the list
 function buscar() {
 	// get text to search by
-	var text = cleanUpSpecialChars($('#buscar').val().toLowerCase());
+	var text = cleanUpSpecialChars($('#buscar input').val().toLowerCase());
 
 	$('.service').show().each(function(i, e) {
 		// get the caption
@@ -85,15 +118,38 @@ function widgetSave() {
 		}
 	});
 
-	// get favorite services
-	var favorites = $('#favoritos').val().slice(0, 4).join(',');
-
 	// submit widgets to be saved
 	apretaste.send({
 		command: 'INICIO SALVAR',
-		data: {
-			'widgets': widgets,
-			'favorites': favorites
-		}
+		data: {'widgets': widgets}
 	});
+}
+
+// save favorite
+function toggleFavorite(service) {
+	// submit favorite
+	apretaste.send({
+		command: 'INICIO FAVORITO',
+		data: {'service': service},
+		'redirect': false
+	});
+
+	// change the favorite class
+	$('#'+service).toggleClass('favorite');
+
+	// update favoritos count
+	updateFavoriteCount();
+}
+
+// update favoritos count
+function updateFavoriteCount() {
+	// get the favorite count
+	var favoriteCount = $('.favorite').length;
+
+	// update the counter
+	$('#favorite-count').html(favoriteCount);
+
+	// show/hide message
+	if(favoriteCount > 0) $('#empty-note').hide();
+	else $('#empty-note').show();
 }
--- service.php
+++ service.php
@@ -19,201 +19,196 @@ class Service
 	{
 		// get list of services
 		$services = Database::query("
-			SELECT A.name, A.caption, A.icon, A.category, IFNULL(B.count, 0) AS count
+			SELECT 
+				A.name, A.caption, A.icon, A.category, 
+				IFNULL(B.count, 0) AS count, 
+				IF(C.id IS NULL, 0, 1) AS favorite
 			FROM service A
-			LEFT JOIN (SELECT * FROM service_alerts WHERE person_id = {$request->person->id}) B
+			LEFT JOIN service_alerts B
 			ON A.name = B.name
+			LEFT JOIN service_favorite C 
+			ON A.name = C.service
 			WHERE A.listed = 1 AND A.active = 1
 			ORDER BY A.name ASC");
+
+		// create categories and favorites
+		$favorites = 0;
+		$categories = [];
+		foreach ($services as $item) {
+			// get the categories
+			if(isset($categories[$item->category])) $categories[$item->category]++;
+			else $categories[$item->category] = 1;
+
+			// count favorites
+			if($item->favorite) $favorites++;
+
+			// cast favorite to boolean
+			$item->favorite = (Bool) $item->favorite;
+		}
+
+		// get list of widgets
+		$preferences = Database::queryFirst("SELECT widgets, week_rank FROM person WHERE id = {$request->person->id}");
 
 		// get data for the widgets
 		$widgets = [];
 
-		// do not get widget data for guest users
-		if (empty($request->person->isGuest)) {
-			// get list of widgets
-			$preferences = Database::queryFirst("SELECT widgets, favorite_services, week_rank FROM person WHERE id = {$request->person->id}");
-
-			// social
-			if (strpos($preferences->widgets, 'social') !== false) {
-				$widgets['social'] = (Object) [
-					'amigos' => count($request->person->getFriends()),
-					'ranking' => $preferences->week_rank,
-					'mensajes' => Chats::unreadCount($request->person->id),
-					'retos' => count(array_filter(Challenges::getList($request->person->id), static function ($ch) {
-						return (int) $ch->done == 0;
-					})),
-				];
-			}
-
-			// online
-			if (strpos($preferences->widgets, 'online') !== false) {
-				$widgets['online'] = (Object) [
-					'desde' => date("d/m/Y", strtotime($request->person->insertionDate)),
-					'semana' => $request->person->daysStreak,
-				];
-			}
-
-			// experiencia
-			if (strpos($preferences->widgets, 'experiencia') !== false) {
-				$widgets['experiencia'] = (Object) [
-					'experiencia' => $request->person->experience,
-					'nivel' => $request->person->level,
-				];
-			}
-
-			// pizarra
-			if (strpos($preferences->widgets, 'pizarra') !== false) {
-				$data = Database::queryFirst("
-					SELECT SUM(likes) AS likes, SUM(comments) AS comments
-					FROM _pizarra_notes
-					WHERE id_person = {$request->person->id}
-					AND inserted >= CURRENT_DATE");
-
-				// set widget
-				$widgets['pizarra'] = (Object) [
-					'like' => empty($data->likes) ? 0 : $data->likes,
-					'comment' => empty($data->comments) ? 0 : $data->comments,
-				];
-			}
-
-			// amuletos
-			if (strpos($preferences->widgets, 'amuletos') !== false) {
-				// get data
-				$data = Database::query("
-					SELECT B.icon
-					FROM _amulets_person A
-					JOIN _amulets B
-					ON A.amulet_id = B.id
-					WHERE A.person_id = {$request->person->id}
-					AND A.active = 1
-					AND (A.expires IS NULL OR A.expires > CURRENT_TIMESTAMP)");
-
-				// set widget
-				$widgets['amuletos'] = (Object) [
-					'one' => empty($data[0]->icon) ? '' : $data[0]->icon,
-					'two' => empty($data[1]->icon) ? '' : $data[1]->icon,
-					'three' => empty($data[2]->icon) ? '' : $data[2]->icon,
-				];
-			}
-
-			// ayuda
-			if (strpos($preferences->widgets, 'ayuda') !== false) {
-				// get data
-				$data = Database::queryFirst("
-					SELECT COUNT(A.id) as cnt
-					FROM support_chats A
-					JOIN support_tickets B ON A.ticket_id = B.id
-					WHERE B.status <> 'CLOSED'
-					AND A.person_id = {$request->person->id}");
-
-				// set widget
-				$widgets['ayuda'] = (Object) ['tickets' => $data->cnt];
-			}
-
-			// rifa
-			if (strpos($preferences->widgets, 'rifa') !== false) {
-				// get the current raffle deadline
-				$data = Database::queryFirst('SELECT end_date FROM raffle WHERE CURRENT_TIMESTAMP BETWEEN start_date AND end_date ORDER BY start_date');
-				$deadline = empty($data->end_date) ? '' : $data->end_date;
-
-				// get number of tickets by the user
-				$data = Database::queryFirst("SELECT COUNT(ticket_id) AS cnt FROM ticket WHERE raffle_id is NULL AND person_id = '{$request->person->id}'");
-				$tickets = (int) $data->cnt;
-
-				// set widgets
-				$widgets['rifa'] = (Object) [
-					'fecha' => $deadline,
-					'tickets' => $tickets,
-				];
-			}
-
-			// bolita
-			if (strpos($preferences->widgets, 'bolita') !== false) {
-				// get data
-				$data = Database::queryFirst("
-					SELECT 
-						corrida, papeleta_day, fijo_day, corrido1_day, corrido2_day, 
-						papeleta_night, fijo_night, corrido1_night, corrido2_night
-				 	FROM _bolita_results ORDER BY corrida DESC");
-
-				// set widgets
-				$widgets['bolita'] = $data;
-			}
-
-			// piropazo
-			if (strpos($preferences->widgets, 'piropazo') !== false) {
-				// get data
-				$parejas = Database::queryFirst("
-					SELECT COUNT(id) AS cnt
-					FROM _piropazo_relationships 
-					WHERE status = 'match'
-					AND (id_from = {$request->person->id} OR id_to = {$request->person->id})");
-
-				$likes = Database::queryFirst("
-					SELECT likes AS cnt
-					FROM _piropazo_people 
-					WHERE id_person = {$request->person->id}");
-
-				// set widgets
-				$widgets['piropazo'] = (Object) [
-					'parejas' => $parejas->cnt,
-					'likes' => empty($likes->cnt) ? 0 : $likes->cnt,
-				];
-			}
-
-			// noticia
-			if (strpos($preferences->widgets, 'noticia') !== false) {
-				// get data
-				$data = Database::queryFirst("
-					SELECT A.id, A.title, A.comments, B.caption AS channel
-					FROM _news_articles A
-					JOIN _news_media B
-					ON A.media_id = B.id
-					WHERE A.inserted >= DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY)
-					ORDER BY A.comments DESC 
-					LIMIT 1");
-
-				// set widget
-				if ($data) {
-					$widgets['noticia'] = (Object) [
-						'id' => $data->id,
-						'titulo' => trim(substr(quoted_printable_decode($data->title), 0, 60)) . '...',
-						'canal' => $data->channel,
-						'comment' => $data->comments,
-					];
-				}
-			}
-
-			// favoritos
-			if (strpos($preferences->widgets, 'favoritos') !== false) {
-				$widgets['favoritos'] = [];
-				foreach ($services as $item) {
-					if (strpos($preferences->favorite_services, $item->name) !== false) {
-						$widgets['favoritos'][] = $item;
-					}
-				}
-			}
+		// social
+		if (strpos($preferences->widgets, 'social') !== false) {
+			$widgets['social'] = (Object) [
+				'amigos' => count($request->person->getFriends()),
+				'ranking' => $preferences->week_rank,
+				'mensajes' => Chats::unreadCount($request->person->id),
+				'retos' => count(array_filter(Challenges::getList($request->person->id), static function ($ch) {
+					return (int) $ch->done == 0;
+				})),
+			];
 		}
 
-		// get services categories
-		$cats = Database::queryCache("
-			SELECT COUNT(*) AS cnt, category 
-			FROM service
-			WHERE listed = 1 AND active = 1
-			GROUP BY category");
+		// online
+		if (strpos($preferences->widgets, 'online') !== false) {
+			$widgets['online'] = (Object) [
+				'desde' => date("d/m/Y", strtotime($request->person->insertionDate)),
+				'semana' => $request->person->daysStreak,
+			];
+		}
 
-		// organize the categories
-		$categories = [];
-		foreach ($cats as $key => $value) {
-			$categories[$value->category] = $value->cnt;
+		// experiencia
+		if (strpos($preferences->widgets, 'experiencia') !== false) {
+			$widgets['experiencia'] = (Object) [
+				'experiencia' => $request->person->experience,
+				'nivel' => $request->person->level,
+			];
+		}
+
+		// pizarra
+		if (strpos($preferences->widgets, 'pizarra') !== false) {
+			$data = Database::queryFirst("
+				SELECT SUM(likes) AS likes, SUM(comments) AS comments
+				FROM _pizarra_notes
+				WHERE id_person = {$request->person->id}
+				AND inserted >= CURRENT_DATE");
+
+			// set widget
+			$widgets['pizarra'] = (Object) [
+				'like' => empty($data->likes) ? 0 : $data->likes,
+				'comment' => empty($data->comments) ? 0 : $data->comments,
+			];
+		}
+
+		// amuletos
+		if (strpos($preferences->widgets, 'amuletos') !== false) {
+			// get data
+			$data = Database::query("
+				SELECT B.icon
+				FROM _amulets_person A
+				JOIN _amulets B
+				ON A.amulet_id = B.id
+				WHERE A.person_id = {$request->person->id}
+				AND A.active = 1
+				AND (A.expires IS NULL OR A.expires > CURRENT_TIMESTAMP)");
+
+			// set widget
+			$widgets['amuletos'] = (Object) [
+				'one' => empty($data[0]->icon) ? '' : $data[0]->icon,
+				'two' => empty($data[1]->icon) ? '' : $data[1]->icon,
+				'three' => empty($data[2]->icon) ? '' : $data[2]->icon,
+			];
+		}
+
+		// ayuda
+		if (strpos($preferences->widgets, 'ayuda') !== false) {
+			// get data
+			$data = Database::queryFirst("
+				SELECT COUNT(A.id) as cnt
+				FROM support_chats A
+				JOIN support_tickets B ON A.ticket_id = B.id
+				WHERE B.status <> 'CLOSED'
+				AND A.person_id = {$request->person->id}");
+
+			// set widget
+			$widgets['ayuda'] = (Object) ['tickets' => $data->cnt];
+		}
+
+		// rifa
+		if (strpos($preferences->widgets, 'rifa') !== false) {
+			// get the current raffle deadline
+			$data = Database::queryFirst('SELECT end_date FROM raffle WHERE CURRENT_TIMESTAMP BETWEEN start_date AND end_date ORDER BY start_date');
+			$deadline = empty($data->end_date) ? '' : $data->end_date;
+
+			// get number of tickets by the user
+			$data = Database::queryFirst("SELECT COUNT(ticket_id) AS cnt FROM ticket WHERE raffle_id is NULL AND person_id = '{$request->person->id}'");
+			$tickets = (int) $data->cnt;
+
+			// set widgets
+			$widgets['rifa'] = (Object) [
+				'fecha' => $deadline,
+				'tickets' => $tickets,
+			];
+		}
+
+		// bolita
+		if (strpos($preferences->widgets, 'bolita') !== false) {
+			// get data
+			$data = Database::queryFirst("
+				SELECT 
+					corrida, papeleta_day, fijo_day, corrido1_day, corrido2_day, 
+					papeleta_night, fijo_night, corrido1_night, corrido2_night
+			 	FROM _bolita_results ORDER BY corrida DESC");
+
+			// set widgets
+			$widgets['bolita'] = $data;
+		}
+
+		// piropazo
+		if (strpos($preferences->widgets, 'piropazo') !== false) {
+			// get data
+			$parejas = Database::queryFirst("
+				SELECT COUNT(id) AS cnt
+				FROM _piropazo_relationships 
+				WHERE status = 'match'
+				AND (id_from = {$request->person->id} OR id_to = {$request->person->id})");
+
+			$likes = Database::queryFirst("
+				SELECT likes AS cnt
+				FROM _piropazo_people 
+				WHERE id_person = {$request->person->id}");
+
+			// set widgets
+			$widgets['piropazo'] = (Object) [
+				'parejas' => $parejas->cnt,
+				'likes' => empty($likes->cnt) ? 0 : $likes->cnt,
+			];
+		}
+
+		// noticia
+		if (strpos($preferences->widgets, 'noticia') !== false) {
+			// get data
+			$data = Database::queryFirst("
+				SELECT A.id, A.title, A.comments, B.caption AS channel
+				FROM _news_articles A
+				JOIN _news_media B
+				ON A.media_id = B.id
+				WHERE A.inserted >= DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY)
+				ORDER BY A.comments DESC 
+				LIMIT 1");
+
+			// set widget
+			if ($data) {
+				$widgets['noticia'] = (Object) [
+					'id' => $data->id,
+					'titulo' => trim(substr(quoted_printable_decode($data->title), 0, 60)) . '...',
+					'canal' => $data->channel,
+					'comment' => $data->comments,
+				];
+			}
 		}
 
 		// create the content array
 		$content = [
 			'widgets' => $widgets,
 			'services' => $services,
-			'categories' => $categories
+			'categories' => $categories,
+			'favorites' => $favorites
 		];
 
 		// create response
@@ -231,7 +226,7 @@ class Service
 	public function _editar(Request $request, Response $response)
 	{
 		// get list of widgets
-		$preferences = Database::queryFirst("SELECT widgets, favorite_services FROM person WHERE id = {$request->person->id}");
+		$preferences = Database::queryFirst("SELECT widgets FROM person WHERE id = {$request->person->id}");
 
 		// get list of services
 		$services = Database::query("SELECT name, caption FROM service WHERE listed = 1 AND active = 1 ORDER BY name ASC");
@@ -239,8 +234,7 @@ class Service
 		// create the content array
 		$content = [
 			'services' => $services,
-			'widgets' => $preferences->widgets,
-			'favorites' => $preferences->favorite_services
+			'widgets' => $preferences->widgets
 		];
 
 		// create response
@@ -258,17 +252,46 @@ class Service
 	{
 		// save the widgets
 		$widgets = implode(',', $request->input->data->widgets);
-		$favorites = $request->input->data->favorites;
 
 		// save widgets to the user profile
-		Database::queryFirst("
-			UPDATE person 
-			SET widgets = '$widgets', favorite_services = '$favorites'
-			WHERE id = {$request->person->id}");
+		Database::query("UPDATE person SET widgets = '$widgets' WHERE id = {$request->person->id}");
 
+		// complete challenge
 		Challenges::complete('edit-widgets', $request->person->id);
 
 		// create response
 		return $this->_main($request, $response);
+	}
+
+	/**
+	 * Toggle a service as favorite
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @author salvipascual
+	 */
+	public function _favorito(Request $request, Response $response)
+	{
+		// check if the service is favorite
+		$isFavorite = Database::queryFirst("
+			SELECT COUNT(A.id) AS cnt
+			FROM service_favorite A
+			JOIN service B
+			ON A.service = B.name
+			WHERE A.person_id = {$request->person->id}
+			AND A.service = '{$request->input->data->service}'");
+
+		if($isFavorite->cnt) {
+			// remove from favorites
+			Database::query("
+				DELETE FROM service_favorite 
+				WHERE person_id = {$request->person->id} 
+				AND service = '{$request->input->data->service}'");
+		} else {
+			// add to favorites
+			Database::query("
+				INSERT INTO service_favorite (person_id, service) 
+				VALUES ({$request->person->id}, '{$request->input->data->service}')");
+		}
 	}
 }
